31e9246eb9a9d7caef2b3371b6edeb155e14c720
javieragustin2702-del <1DAM@A11-PC13>
2025-11-17 11:27:50 +0100

    asdf

---
+++ reto1JavierAgustinGarciaBoleaFabricioCaceresAlexDanielMusca/src/reto/funciones1.java
@@ -0,0 +1,10 @@
+package reto;
+
+public class funciones1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ reto1JavierAgustinGarciaBoleaFabricioCaceresAlexDanielMusca/src/reto/funciones2.java
@@ -0,0 +1,10 @@
+package reto;
+
+public class funciones2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
