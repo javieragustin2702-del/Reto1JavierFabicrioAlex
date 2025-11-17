1702576cfa2bec4fb2a130f4ac76a792473753eb
1DAM <1DAM@A11-PC05>
2025-11-17 13:13:00 +0100

    Funciones 3

--- reto1JavierAgustinGarciaBoleaFabricioCaceresAlexDanielMusca/src/reto/funciones3.java
+++ reto1JavierAgustinGarciaBoleaFabricioCaceresAlexDanielMusca/src/reto/funciones3.java
@@ -1,5 +1,30 @@
 package reto;
 
+
 public class funciones3 {
 
+	public static int multiplicar(int a, int b) {
+		return a * b;
+	}
+
+	public static int dividir(int a, int b) {
+		if (b<0) {
+			return 0;
+		}
+		return a / b;
+	}
+	public static boolean EsPositivo(int n) {
+		if (n>=0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public static boolean EsNegativo(int n) {
+		if (n<0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
